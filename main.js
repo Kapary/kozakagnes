6328e27c8d71425fa5b7d33b90337e5699ee881f
Karol Partyka <kpar@gdnwin10agli.stibo.corp>
2020-08-01 13:24:11 +0200

    fixed insta feed and added link to insta, smooth scrolling for menu

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-window.onload = function() {
+window.onload = function () {
   const offerLinks = document.querySelectorAll(".stories-list-item-link");
-  offerLinks.forEach(function(item) {
-    item.addEventListener("mouseover", function() {
-      offerLinks.forEach(function(item) {
+  offerLinks.forEach(function (item) {
+    item.addEventListener("mouseover", function () {
+      offerLinks.forEach(function (item) {
         item.classList.add("faded");
       });
 
       this.classList.remove("faded");
     });
 
-    item.addEventListener("mouseout", function() {
-      offerLinks.forEach(function(item) {
+    item.addEventListener("mouseout", function () {
+      offerLinks.forEach(function (item) {
         item.classList.remove("faded");
       });
     });
@@ -18,7 +18,7 @@ window.onload = function() {
 
   document
     .getElementById("mobile-menu-trigger")
-    .addEventListener("click", function() {
+    .addEventListener("click", function () {
       this.classList.toggle("active");
       document
         .getElementById("mobile-menu-wrapper")
@@ -27,10 +27,22 @@ window.onload = function() {
 
   document
     .getElementById("mobile-menu-wrapper")
-    .addEventListener("click", function() {
+    .addEventListener("click", function () {
       document
         .getElementById("mobile-menu-wrapper")
         .classList.remove("visible");
       document.getElementById("mobile-menu-trigger").classList.remove("active");
     });
+
+  // smooth scrolling for main menu
+  var mainMenus = document.querySelectorAll(".main-menu");
+
+  mainMenus.forEach(function (mainMenu) {
+    mainMenu.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.querySelector(e.target.hash).scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+  });
 };
